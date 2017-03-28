5a90c2190106017e3779d6490bce7f9472c561e4
ann <ann@ann.ub16.ann>
2017-03-28 09:41:22 -0700

    hello

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run()
